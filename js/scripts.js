aec475d1be976b413f93529934428818611d09d9
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-01-03 22:57:09 -0300

    finalizando projeto

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 const formTodo = document.querySelector(".todo");
 const inputToDo = document.querySelector("#todo-list");
-const result = document.querySelector("#result-todo");
+const result = document.querySelector(".result-todo");
 
 const criaDiv = (tarefa) => {
   const inputValue = inputToDo.value;
@@ -9,36 +9,51 @@ const criaDiv = (tarefa) => {
   tarefa.textContent = inputValue;
 
   //Div Pai chamada buttons
-  const divPai = document.createElement("div")
-  divPai.classList.add("buttons")
-  tarefa.appendChild(divPai)
+  const divPai = document.createElement("div");
+  divPai.classList.add("buttons");
+  tarefa.appendChild(divPai);
 
   //criando um botão de com icone de apagar
-  const apagar = document.createElement("button")
-  divPai.appendChild(apagar)
+  const apagar = document.createElement("button");
+  apagar.classList.add("button_apagar");
+  divPai.appendChild(apagar);
 
-  const iconApagar = document.createElement("i")
+  const iconApagar = document.createElement("i");
   iconApagar.classList.add("fas", "fa-trash-alt");
   apagar.appendChild(iconApagar);
 
-  //criando um botão de com icone de editar
-  const edit = document.createElement("button");
-  divPai.appendChild(edit)
+  //criando um botão de com icone de marcar como feito
+  const feito = document.createElement("button");
+  feito.classList.add("button_marcar");
+  divPai.appendChild(feito);
 
-  const icon = document.createElement("i")
-  icon.classList.add("fas", "fa-pen-to-square");
-  edit.appendChild(icon);
+  const iconFeito = document.createElement("i");
+  iconFeito.classList.add("fas", "fa-check");
+  feito.appendChild(iconFeito);
 
   tarefa.classList.add("mostrar");
   result.appendChild(tarefa);
 
-  inputToDo.value = "";
+  apagar.addEventListener("click", function () {
+    tarefa.remove();
+  });
 
-}
+  feito.addEventListener("click", function () {
+    tarefa.classList.add("feita");
+    result.appendChild(tarefa);
+  });
+
+  inputToDo.value = "";
+};
 
 formTodo.addEventListener("submit", (e) => {
   e.preventDefault();
-  // Create an element div for the new task
+
+  if (inputToDo.value.trim() === "") {
+    alert("Digite sua tarefa");
+    return;
+  }
+
   const tarefa = document.createElement("div");
   criaDiv(tarefa);
 });
